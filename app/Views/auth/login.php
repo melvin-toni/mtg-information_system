6595079e767c5810d4e9b2a6139f95ce95c34ab1
Melvin <melvin@tractive.com.my>
2020-07-14 06:41:39 +0800

    MIS2 - fix login ui

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -20,26 +20,50 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-5">
+            <div class="row row-cols-4 justify-content-center">
+                <div class="col main_bg_color form_login">
                     <form method="post" action="login/validate">
+                        <?php if (esc(session()->get('not_found'))) : ?>
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>Login gagal !</strong> <?=esc(session()->getFlashdata('not_found'));?>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        <?php endif ?>
+                        <h1>Sistem Informasi</h1>
                         <?=csrf_field()?>
                         <div class="form-group row">
-                            <label for="inputUser" class="col-sm-2 col-form-label">Email</label>
-                            <div class="col-sm-10">
-                                <?=esc(session()->getFlashdata('not_found'));?>
-                                <input type="text" class="form-control" id="inputUser" name="inputUser" value="admin@gmail.com">
-                                <?=esc(\Config\Services::validation()->getError('inputUser'))?>
+                            <label for="inputUser" class="offset-1 col-sm-1 col-form-label"><i class="fas fa-envelope"></i></label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="inputUser" name="inputUser" placeholder="Masukkan email">
+                                <small id="userHelpBlock" class="form-text help_block_font">
+                                    <?=esc(\Config\Services::validation()->getError('inputUser'))?>
+                                </small>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control" id="inputPassword" name="inputPassword">
-                                <?=esc(\Config\Services::validation()->getError('inputPassword'))?>
+                            <label for="inputPassword" class="offset-1 col-sm-1 col-form-label"><i class="fas fa-key"></i></label>
+                            <div class="col-sm-9">
+                                <input type="password" class="form-control" id="inputPassword" name="inputPassword" placeholder="Masukkan password">
+                                <small id="passHelpBlock" class="form-text help_block_font">
+                                    <?=esc(\Config\Services::validation()->getError('inputPassword'))?>
+                                </small>
                             </div>
                         </div>
-                        <input type="submit" class="btn btn-primary" value="Login">
+                        <div class="form-group row">
+                            <div class="offset-2 col-11">
+                                <input type="submit" class="btn btn-success" value="Login">
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="row footer">
+                                <p>Copyright © 2020 |</p>
+                                <a href="<?=getenv('creator.linkedin');?>" class="pl-1">
+                                    <?=getenv('creator.name');?>    
+                                </a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
